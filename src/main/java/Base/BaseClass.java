7eb9747357e44c2d7297a08283569f5b603d0b72
Mukesh-Kaira <33417598+Mukesh-Kaira@users.noreply.github.com>
2018-09-27 12:13:35 +0530

    Remove modify header code from base class
    Remove modify header method code from base and create an utility for that under utili package

--- src/main/java/Base/BaseClass.java
+++ src/main/java/Base/BaseClass.java
@@ -60,39 +60,8 @@ public class BaseClass {
 	driver.manage().deleteAllCookies();
 	}
 	
-	public static void modifyheader() throws InterruptedException
-	{
-		//System.out.println(driver);
-		driver.get("chrome-extension://innpjfdalfhpcoinfnehdnbkglpmogdi/options.html");
-		driver.navigate().refresh();
-		Thread.sleep(1000);
-		
-		driver.findElement(By.xpath("//button[@tooltip='Add New']")).click();
-		
-		WebElement actionElem = driver.findElement(By.name("action"));
-		Select select = new Select(actionElem);
-		select.selectByVisibleText("Add");
-		
-		WebElement nameElem = driver.findElement(By.name("name"));
-		nameElem.sendKeys("username");
-
-		WebElement valueElem = driver.findElement(By.name("value"));
-		valueElem.sendKeys("csr2@hcentive.com");
-
-		WebElement descElem = driver.findElement(By.name("description"));
-		descElem.sendKeys("backoffice");
-
-		driver.findElement(By.xpath("//button[@tooltip='Save']")).click();
-		Thread.sleep(1000);
-		
-		driver.findElement(By.xpath("//button[@tooltip='Start Modifying Headers']")).click();
-		Thread.sleep(1000);
-		
-		driver.findElement(By.xpath("//button[@tooltip='Enable']")).click();
-		Thread.sleep(2000);
-		
-        
-	}
+	
+	
 	
 	
 	
